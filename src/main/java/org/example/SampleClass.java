2f8be98d76a532b4aa5d736aa2725d338a3b816b
Schroderwin <kevin31240@gmail.com>
2023-01-24 14:21:16 +0100

    Add on test

---
+++ src/main/java/org/example/SampleClass.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+
+public class SampleClass {
+    private int number;
+
+    public SampleClass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
